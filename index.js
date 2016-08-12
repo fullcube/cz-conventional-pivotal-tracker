efe772cb472930c104fbca74759ebbe172e1835b
Bram Borggreve <borggreve@gmail.com>
2016-08-12 15:58:09 +0200

    feat: Automatically parse the PT story ID based on the git branch [#94942650]

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const wrap = require('word-wrap')
 const types = require('conventional-commit-types').types
 const padend = require('lodash.padend')
 const startsWith = require('lodash.startswith')
+const git = require('git-rev')
 
 function readConfigFile() {
   // Try to find config block in the nearest package.json
@@ -36,6 +37,13 @@ function getScopes(config) {
   return config && config.scopes || []
 }
 
+function getPivotalTrackerId(cb) {
+  git.branch(function (str) {
+    const parts = str.split('-')
+    const storyId = parts[parts.length - 1]
+    cb(`#${storyId}`)
+  })
+}
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
 // fine.
@@ -57,111 +65,114 @@ module.exports = {
     console.log(config)
     console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n')
 
-    // Let's ask some questions of the user
-    // so that we can populate our commit
-    // template.
-    //
-    // See inquirer.js docs for specifics.
-    // You can also opt to use another input
-    // collection library if you prefer.
-    cz.prompt([
-      {
-        type: 'list',
-        name: 'type',
-        message: 'Select the type of change that you\'re committing:',
-        choices: getTypes(config),
-      }, {
-        type: 'list',
-        name: 'scope',
-        message: 'Denote the scope of this change (devops, app, server, etc.):',
-        choices: getScopes(config).concat([
-          new cz.Separator(),
-          { name: 'empty', value: false },
-          { name: 'custom', value: 'custom' },
-        ]),
-        when: function(answers) {
-          return getScopes(config).length
-        }
-      }, {
-        type: 'input',
-        name: 'scope',
-        message: 'Denote the SCOPE of this change:',
-        when: function(answers) {
-          return !getScopes(config).length || answers.scope === 'custom'
-        }
-      }, {
-        type: 'input',
-        name: 'subject',
-        message: 'Write a short, imperative tense description of the change:\n'
-      }, {
-        type: 'input',
-        name: 'body',
-        message: 'Provide a longer description of the change:\n'
-      }, {
-        type: 'input',
-        name: 'footer',
-        message: 'List any breaking changes:\n'
-      }, {
-        type: 'input',
-        name: 'story',
-        message: 'Pivotal Tracker Story ID:\n',
-        validate: function(input) {
-          if (input && !startsWith(input, '\#')) {
-            return 'Pivotal Tracker Story ID must start with \'#\'';
-          } else {
-            return true;
+    getPivotalTrackerId(function(storyId) {
+      // Let's ask some questions of the user
+      // so that we can populate our commit
+      // template.
+      //
+      // See inquirer.js docs for specifics.
+      // You can also opt to use another input
+      // collection library if you prefer.
+      cz.prompt([
+        {
+          type: 'list',
+          name: 'type',
+          message: 'Select the type of change that you\'re committing:',
+          choices: getTypes(config),
+        }, {
+          type: 'list',
+          name: 'scope',
+          message: 'Denote the scope of this change (devops, app, server, etc.):',
+          choices: getScopes(config).concat([
+            new cz.Separator(),
+            { name: 'empty', value: false },
+            { name: 'custom', value: 'custom' },
+          ]),
+          when: function(answers) {
+            return getScopes(config).length
+          }
+        }, {
+          type: 'input',
+          name: 'scope',
+          message: 'Denote the SCOPE of this change:',
+          when: function(answers) {
+            return !getScopes(config).length || answers.scope === 'custom'
+          }
+        }, {
+          type: 'input',
+          name: 'subject',
+          message: 'Write a short, imperative tense description of the change:\n'
+        }, {
+          type: 'input',
+          name: 'body',
+          message: 'Provide a longer description of the change:\n'
+        }, {
+          type: 'input',
+          name: 'footer',
+          message: 'List any breaking changes:\n'
+        }, {
+          type: 'input',
+          name: 'story',
+          message: 'Pivotal Tracker Story ID:\n',
+          default: storyId,
+          validate: function(input) {
+            if (input && !startsWith(input, '\#')) {
+              return 'Pivotal Tracker Story ID must start with \'#\'';
+            } else {
+              return true;
+            }
+          }
+        }, {
+          type: 'list',
+          name: 'workflow',
+          message: 'Workflow command (testing, closed, etc.) (optional):\n',
+          choices: [
+            {name: 'Finishes'},
+            {name: 'Fixes'},
+            {name: 'Delivers'},
+            new cz.Separator(),
+            { name: 'none', value: false },
+          ],
+          when: function(answers) {
+            return answers.story
           }
         }
-      }, {
-        type: 'list',
-        name: 'workflow',
-        message: 'Workflow command (testing, closed, etc.) (optional):\n',
-        choices: [
-          {name: 'Finishes'},
-          {name: 'Fixes'},
-          {name: 'Delivers'},
-          new cz.Separator(),
-          { name: 'none', value: false },
-        ],
-        when: function(answers) {
-          return answers.story
+      ]).then(function(answers) {
+
+        var maxLineWidth = 100
+        var headTrimLength = maxLineWidth
+
+        var wrapOptions = {
+          trim: true,
+          newline: '\n',
+          indent:'',
+          width: maxLineWidth
         }
-      }
-    ]).then(function(answers) {
 
-      var maxLineWidth = 100
-      var headTrimLength = maxLineWidth
+        // parentheses are only needed when a scope is present.
+        var scope = answers.scope.trim()
+        scope = scope ? '(' + answers.scope.trim() + ')' : ''
 
-      var wrapOptions = {
-        trim: true,
-        newline: '\n',
-        indent:'',
-        width: maxLineWidth
-      }
+        // Hard limit this line
+        var subject = answers.type + scope + ': ' + answers.subject.trim()
+        var pt = ''
 
-      // parentheses are only needed when a scope is present.
-      var scope = answers.scope.trim()
-      scope = scope ? '(' + answers.scope.trim() + ')' : ''
+        // Add the story Pivotal Tracker story ID.
+        if (answers.story) {
+          const workflow = answers.workflow ? `${answers.workflow} ` : ''
+          pt = ` [${workflow}${answers.story}]`
+          headTrimLength = maxLineWidth - pt.length
+        }
 
-      // Hard limit this line
-      var subject = answers.type + scope + ': ' + answers.subject.trim()
-      var pt = ''
+        // Hard limit this line.
+        var head = (subject).slice(0, headTrimLength) + pt
 
-      // Add the story Pivotal Tracker story ID.
-      if (answers.story) {
-        const workflow = answers.workflow ? `${answers.workflow} ` : ''
-        pt = ` [${workflow}${answers.story}]`
-        headTrimLength = maxLineWidth - pt.length
-      }
+        // Wrap these lines at 100 characters.
+        var body = wrap(answers.body, wrapOptions)
+        var footer = wrap(answers.footer, wrapOptions)
 
-      // Hard limit this line.
-      var head = (subject).slice(0, headTrimLength) + pt
-
-      // Wrap these lines at 100 characters.
-      var body = wrap(answers.body, wrapOptions)
-      var footer = wrap(answers.footer, wrapOptions)
-
-      commit(head + '\n\n' + body + '\n\n' + footer)
+        commit(head + '\n\n' + body + '\n\n' + footer)
+      })
     })
   }
 }
